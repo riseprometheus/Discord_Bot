c23199bce68c1fec1eb65932e7007f55ab2a77cc
Chris Bilbo <ChristopherBilbo@gmail.com>
2018-02-09 18:38:28 -0500

    Voice commands

--- Commands/getFunctionMap.js
+++ Commands/getFunctionMap.js
@@ -3,11 +3,15 @@ exports.run = (command) => {
                 "time":"Misc",
                 "guilddetails":"Misc",
                 "sweep":"Misc",
+                "killme":"Misc",
                 //Moderation Commands
                 "getroles": "Moderation",
                 "nickname": "Moderation",
                 //Music
                "summon": "Music",
-               "leave": "Music"};
+               "leave": "Music",
+               //hank
+               "hankhello":"Hank",
+               "hankwhite":"Hank"};
     return dict[command];
  }
