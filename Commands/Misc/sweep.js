affb526cbb61322d9c390b1e612d13e00a880032
Chris Bilbo <ChristopherBilbo@gmail.com>
2018-02-14 14:48:06 -0500

    Updating sweep command to have default and user parameter options

--- Commands/Misc/sweep.js
+++ Commands/Misc/sweep.js
@@ -4,7 +4,20 @@ exports.run = (client, message,args) => {
        message.channel.send("You don't have high enough permissions to do that.").catch(console.error);
        return;
      }
-     message.channel.send("Attempting to delete last 20 messages.").catch(console.error).then(
-       message.channel.send("You don't have high enough permissions to do that."));
-     message.channel.bulkDelete(20).catch(error => console.log(error.stack));
+     var numberToDelete = 20;
+     if(args.length != 0){
+       if(args[0]<2)
+       {
+         message.channel.send("Must require 2 or more messages to be deleted.").catch(console.error);
+         return;
+       }
+       numberToDelete = args[0];
+     }
+     message.channel.send("Attempting to delete last " + numberToDelete+" messages.").catch(console.error);
+     message.channel.bulkDelete(numberToDelete).catch(function(error) {
+       console.log(error.stack);
+       message.channel.send("Something went wrong when deleting messages. Check permissions and try again.");
+       return;
+     });
+     message.channel.send("The last " + numberToDelete+" messages have been deleted").catch(console.error);
  }
