a8d58cfe37bd1d06a08957e62d5dfa0399c964bd
Chris Bilbo <christopherbilbo@gmail.com>
2019-06-07 15:51:29 -0400

    Better error

--- Commands/Moderation/channelsize.js
+++ Commands/Moderation/channelsize.js
@@ -1,5 +1,10 @@
 exports.run = (client, message,args) => {
   var channelID = message.member.voiceChannelID;
+  if(channelID!== 'undefined'){
+    message.reply("Please be in a voice channel to set it's user limit.");
+    return;
+  }
+
   console.log(`${channelID}`);
   message.guild.channels.get(channelID).setUserLimit(args[0]);
   return;
