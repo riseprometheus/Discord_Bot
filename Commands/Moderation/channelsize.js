0880dc4632bd5afe47ddc14d4cec65e4eb56fc96
Chris Bilbo <christopherbilbo@gmail.com>
2019-06-07 15:46:23 -0400

    lower case

--- Commands/Moderation/channelsize.js
+++ Commands/Moderation/channelsize.js
@@ -2,4 +2,5 @@ exports.run = (client, message,args) => {
   var channelID = message.member.voiceChannelID;
   console.log(`${channelID}`);
   message.guild.channels.get(channelID).setUserLimit(args[0]);
+  return;
 }
