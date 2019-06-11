8edfb91dc9b1b1431ca5d5f0ba92e1856aadca55
Chris Bilbo <christopherbilbo@gmail.com>
2019-06-11 18:08:16 -0400

    Debug

--- Commands/Moderation/move.js
+++ Commands/Moderation/move.js
@@ -6,6 +6,7 @@ exports.run = (client, message,args) => {
   }
 
   var collectionKey = message.mentions.members.firstKey();
+  console.log(message.mentions.members.get(collectionKey).voiceChannelID)
   if(message.mentions.members.get(collectionKey).voiceChannelID === null){
     message.reply("Please make sure target user and yourself are in a voice channel.");
     return;
