0979fdb2b340fcb577d859b1a8fcbf8dae7d6dc3
Chris <christopherbilbo@gmail.com>
2021-04-01 01:50:41 -0400

    Having channel size command respond to you and some more linting

--- Commands/Moderation/channelsize.js
+++ Commands/Moderation/channelsize.js
@@ -1,11 +1,13 @@
-exports.run = (client, message,args) => {
-  var channelID = message.member.voice.ChannelID;
-  if(channelID === null){
-    message.reply("Please be in a voice channel to set its user limit.");
-    return;
+exports.run = (client, message, args) => {
+  const responseTemplate = require('../../bot.js')
+  const responseEmbed = responseTemplate.getEmbedTemplate()
+
+  var channelID = message.member.voice.ChannelID
+  if (channelID === null) {
+    message.reply('Please be in a voice channel to set its user limit.')
+    return
   }
 
-  console.log(`${channelID}`);
-  message.member.voice.channel.setUserLimit(args[0]);
-  return;
+  message.member.voice.channel.setUserLimit(args[0]).then(
+    message.channel.send(responseEmbed.setTitle('Success!').setDescription(`Channel: ${message.member.voice.channel.name} size has been set to ${args[0]}!`)))
 }
